b021fb2727a27746f62eda9e6acf16cffdfa9d37
RevyBr <irafasz5@gmail.com>
2026-08-07 15:05:59 -0300

    update dnv mds

--- data.js
+++ data.js
@@ -75,6 +75,8 @@ const staticData = {
             { name: "Frieren",              image: "imagens/characters/frieren.webp"       },
             { name: "Yennefer de Vengerberg", image: "imagens/characters/yennefer.webp"   },
             { name: "Claire Redfield", image: "imagens/characters/claire.webp"   },
+             ]},
+            
         { icon: "fas fa-music", items: [
             { name: "Renaissance",      image: "imagens/albums/Renaissance.webp"     },
             { name: "Shikabane no Odori",      image: "imagens/albums/ShikabaneNoOdori.webp"     },
